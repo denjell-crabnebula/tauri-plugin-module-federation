31451a3689c7008218da98e9580ce6886c6ba862
Jakub Romańczyk <j.romanczyk@gmail.com>
2026-04-09 18:55:17 +0200

    chore: migrate example from rspack to rsbuild (#9)

---
+++ example/host/rsbuild.config.ts
@@ -0,0 +1,42 @@
+import { pluginModuleFederation } from '@module-federation/rsbuild-plugin';
+import { defineConfig } from '@rsbuild/core';
+import { pluginReact } from '@rsbuild/plugin-react';
+import { withZephyr } from 'zephyr-rsbuild-plugin';
+
+const useZephyr = process.env.WITH_ZEPHYR === 'true';
+
+export default defineConfig({
+	plugins: [
+		pluginReact(),
+		pluginModuleFederation(
+			{
+				name: 'example_host',
+				filename: 'remoteEntry.js',
+				remotes: {
+					example_guest:
+						'example_guest@http://localhost:3002/remoteEntry.js',
+				},
+				runtimePlugins: ['@crabnebula-dev/tauri-module-federation'],
+				shared: {
+					react: { eager: true },
+					'react-dom': { eager: true },
+					lodash: {},
+				},
+			},
+			{},
+		),
+		useZephyr && withZephyr(),
+	].filter(Boolean),
+	dev: {
+		assetPrefix: 'auto',
+	},
+	output: {
+		assetPrefix: 'auto',
+	},
+	server: {
+		port: 3001,
+	},
+	html: {
+		template: './public/index.html',
+	},
+});
